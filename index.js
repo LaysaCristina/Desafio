c74583714dc6ce4890b0bcfbe8175ca54fe3577a
LaysaCristina <laysacristinadecarvalho@gmail.com>
2026-04-20 10:49:22 -0300

    segunda versão

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 let nome = "João"
-let XP = 3000
+let XP =9000
 let nivel = ""
-if (XP > 0 && XP < 999) {
+if (XP < 1000) {
     nivel = "Ferro"
-} else if (XP > 999 && XP < 2000) {
-    nivel = "bronze"
-} else if (XP > 1999 && XP < 5000){
-    nivel = "prata"
-}else if (XP > 2999 && XP < 6000){
-    nivel = "ouro"
-}else if (XP > 5999 && XP < 7000){
-    nivel = "platina"
-}else if (XP > 6999 && XP < 8000) {
+} else if (XP < 2000) {
+    nivel = "Bronze"
+} else if (XP < 5000){
+    nivel = "Prata"
+}else if (XP < 6000){
+    nivel = "Ouro"
+}else if (XP < 7000){
+    nivel = "Platina"
+}else if (XP < 8000) {
     nivel = "Ascendente"
-}else if (XP > 7999 && XP < 9000){
+}else if (XP < 9000){
     nivel = "Imortal"
-}else if (XP > 8999 && XP < 10001) {
+}else if (XP < 10001) {
     nivel = "Radiante"
 }
 console.log("O herói de nome " + nome + " está no nível " + nivel)
